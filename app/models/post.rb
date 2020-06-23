a451f3a08a27fd2e5bb08339e2e7d2e1fb6a6cd7
Luke Chen <luke.chen.2010@gmail.com>
2020-06-23 01:59:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
  
   def categories_attributes=(category_attributes)
-      if !category_attribute[:name].blank?
+      if category_attribute["name"].present?
         category = Category.find_or_create_by(name: category_attribute[:name])
         self.categories << category
       end
